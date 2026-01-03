072cdd45058fdb3b8f747251769c772aae69e246
Stefan Vogt-Geisse <savogt@protonmail.com>
2026-01-03 01:11:20 +0100

    Bump version to 0.4.0

--- src/kudi/__init__.py
+++ src/kudi/__init__.py
@@ -1,14 +1,9 @@
 """Public API for the Kudi Gaussian IRC parser."""
 
-from importlib import metadata as importlib_metadata
-
 from .analysis.irc_path import IRCPath
 from .exceptions import IncompleteJobError, ParseError, UnsupportedFormatError
 
-try:
-    __version__ = importlib_metadata.version(__package__ or __name__)
-except importlib_metadata.PackageNotFoundError:  # pragma: no cover - fallback when not installed
-    __version__ = "0.0.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "IRCPath",
